eb3b404018efeaa5a3a644a7986e2e4a67244a1d
moreauf <moreauf@NGT15.theobald-groupe.local>
2020-10-14 12:56:04 +0200

    init

--- lib/2l.lib.php
+++ lib/2l.lib.php
@@ -23,33 +23,3 @@
  *				Put some comments here
  */
 
-function listincsvAdminPrepareHead()
-{
-    global $langs, $conf;
-
-    $langs->load("listincsv@listincsv");
-
-    $h = 0;
-    $head = array();
-
-    $head[$h][0] = dol_buildpath("/listincsv/admin/listincsv_setup.php", 1);
-    $head[$h][1] = $langs->trans("Parameters");
-    $head[$h][2] = 'settings';
-    $h++;
-    $head[$h][0] = dol_buildpath("/listincsv/admin/listincsv_about.php", 1);
-    $head[$h][1] = $langs->trans("About");
-    $head[$h][2] = 'about';
-    $h++;
-
-    // Show more tabs from modules
-    // Entries must be declared in modules descriptor with line
-    //$this->tabs = array(
-    //	'entity:+tabname:Title:@listincsv:/listincsv/mypage.php?id=__ID__'
-    //); // to add new tab
-    //$this->tabs = array(
-    //	'entity:-tabname:Title:@listincsv:/listincsv/mypage.php?id=__ID__'
-    //); // to remove a tab
-    complete_head_from_modules($conf, $langs, $object, $head, $h, 'listincsv');
-
-    return $head;
-}
