8e1958a46f1950c1d3d71866262641b9ca0da97b
moreauf <moreauf@NGT15.theobald-groupe.local>
2020-10-21 16:17:28 +0200

    c

--- admin/notify_plus_admin.php
+++ admin/notify_plus_admin.php
@@ -27,9 +27,9 @@
 require '../config.php';
 require_once DOL_DOCUMENT_ROOT.'/core/lib/admin.lib.php';
 require_once DOL_DOCUMENT_ROOT.'/core/class/html.form.class.php';
-require_once DOL_DOCUMENT_ROOT.'/custom/lltrucks/core/triggers/interface_49_modlltrucks_lltruckstrigger.class.php';
-//dol_include_once('lltrucks/lib/lltrucks.lib.php');
-//dol_include_once('lltrucks/class/notify_plus.class.php');
+//require_once DOL_DOCUMENT_ROOT.'/custom/lltrucks/core/triggers/interface_49_modlltrucks_lltruckstrigger.class.php';
+dol_include_once('lltrucks/lib/lltrucks.lib.php');
+dol_include_once('lltrucks/class/notify_plus.class.php');
 
 // // Load translation files required by the page
 // $langs->loadLangs(array('admin', 'other', 'orders', 'propal', 'bills', 'errors', 'mails'));
