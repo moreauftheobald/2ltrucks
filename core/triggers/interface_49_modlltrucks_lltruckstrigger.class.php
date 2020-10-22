427a352e1a31bbf2cf8365a98477f797fd6201a2
moreauf <moreauf@NGT15.theobald-groupe.local>
2020-10-22 12:03:13 +0200

    s

--- core/triggers/interface_49_modlltrucks_lltruckstrigger.class.php
+++ core/triggers/interface_49_modlltrucks_lltruckstrigger.class.php
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-require_once DOL_DOCUMENT_ROOT.'/core/triggers/dolibarrtriggers.class.php';
+//require_once DOL_DOCUMENT_ROOT.'/core/triggers/dolibarrtriggers.class.php';
 
 
 
