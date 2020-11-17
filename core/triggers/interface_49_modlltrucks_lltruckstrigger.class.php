40dfe281ecea9f052aa23724ed417ef5e52c814c
moreauf <moreauf@NGT15.theobald-groupe.local>
2020-11-17 09:20:56 +0100

    debug

--- core/triggers/interface_49_modlltrucks_lltruckstrigger.class.php
+++ core/triggers/interface_49_modlltrucks_lltruckstrigger.class.php
@@ -117,10 +117,10 @@ class Interfacelltruckstrigger
      	if($action == 'TICKET_ASSIGNED' && !empty($object->fk_user_assign)){
         	global $mysoc;
         	
-        	require '../htdocs/main.inc.php';
-        	require_once DOL_DOCUMENT_ROOT. '/core/class/CMailFile.class.php';
-        	require_once DOL_DOCUMENT_ROOT. '/core/lib/files.lib.php';
-        	require_once DOL_DOCUMENT_ROOT. '/user/class/user.class.php';
+        	//require '../htdocs/main.inc.php';
+        	//require_once DOL_DOCUMENT_ROOT. '/core/class/CMailFile.class.php';
+        	//require_once DOL_DOCUMENT_ROOT. '/core/lib/files.lib.php';
+        	//require_once DOL_DOCUMENT_ROOT. '/user/class/user.class.php';
         	        	
 //         	$langs->load("other");
 //         	$langs->load("lltrucks@lltrucks");
